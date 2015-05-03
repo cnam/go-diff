3a23bccac29650e29701d9d041ab81ced97690a5
cnam <cnam812@gmail.com>
2015-05-03 12:46:36 +0300

    example diff files

--- diff.go
+++ diff.go
@@ -4,25 +4,31 @@ import (
     "fmt"
     "github.com/sergi/go-diff/diffmatchpatch"
     "net/http"
+    "io/ioutil"
     "github.com/gin-gonic/gin"
 )
 
 func main() {
     fmt.Println("Hello world")
-    router := gin.Default();
+    router := gin.Default()
+    dmp := diffmatchpatch.New()
+    s1 := readFile("examples/speedtest1.txt")
+    s2 := readFile("examples/speedtest2.txt")
+
     router.GET("/", func(c *gin.Context) {
-        dmp := diffmatchpatch.New()
-        diffs := []diffmatchpatch.Diff{
-            diffmatchpatch.Diff{diffmatchpatch.DiffEqual, "jump"},
-            diffmatchpatch.Diff{diffmatchpatch.DiffDelete, "s"},
-            diffmatchpatch.Diff{diffmatchpatch.DiffInsert, "ed"},
-            diffmatchpatch.Diff{diffmatchpatch.DiffEqual, " over "},
-            diffmatchpatch.Diff{diffmatchpatch.DiffDelete, "the"},
-            diffmatchpatch.Diff{diffmatchpatch.DiffInsert, "a"},
-            diffmatchpatch.Diff{diffmatchpatch.DiffEqual, " lazy"}}
+
+        diffs := dmp.DiffMain(s1, s2, true)
 
         c.HTMLString(http.StatusOK, dmp.DiffPrettyHtml(diffs))
     })
 
     router.Run(":8080")
+}
+
+func readFile(filename string) string {
+    bytes, err := ioutil.ReadFile(filename)
+    if err != nil {
+        
+    }
+    return string(bytes)
 }
